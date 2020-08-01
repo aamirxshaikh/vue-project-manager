249c1bc1e1db935ca35bd5820aa3a0b8b8037018
Aamir shaikh <aamirshaikh3232@gmail.com>
2020-08-01 14:07:07 +0530

    adding navigation drawers

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,31 @@
 <template>
     <nav>
-        <v-toolbar>
-            <v-toolbar-title>
+        <v-app-bar flat app>
+            <v-app-bar-nav-icon @click="show = !show"></v-app-bar-nav-icon>
+            <v-app-bar-title>
                 <span class="font-weight-light">Project Manager</span>
                 <span>Pro</span>
                 <i class="material-icons mx-1">library_add_check</i>
-            </v-toolbar-title>
+            </v-app-bar-title>
             <v-spacer></v-spacer>
             <v-btn color="red darken-1">
                 <span>SIGN OUT</span>
                 <i class="material-icons mx-1">undo</i>
             </v-btn>
-        </v-toolbar>
+        </v-app-bar>
+        <v-navigation-drawer absolute v-model="show" class="deep-grey accent-4">
+            <p>text</p>
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            show: false
+        }
+    }
 }
 </script>
 
